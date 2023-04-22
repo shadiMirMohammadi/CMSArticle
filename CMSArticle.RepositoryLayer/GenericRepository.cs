376079a8c9b71a30261ae747ca7caa7c6db926a5
shadiMirMohammadi <shadi.Mir.Mohammadi.80@gmail.com>
2023-04-22 20:12:58 +0430

    feat(core) : Create a class called GenericRepository
    The GenericRepository class is a child of the IGenericRepository interface because the patterns define here are suppose to be writte as operations

---
+++ CMSArticle.RepositoryLayer/GenericRepository.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using CMSArticle.ModelsLayer.Context;
+using CMSArticle.ModelsLayer;
+
+namespace CMSArticle.RepositoryLayer
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        CMSContext db;
+        DbSet<T> dbContext;
+
+        public GenericRepository(CMSContext context)
+        {
+            db = context;
+            dbContext = context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return dbContext.ToList();
+        }
+
+        public T GetEntity(int id)
+        {
+            return dbContext.Find(id);
+        }
+
+        public bool Add(T entity)
+        {
+            try
+            {
+                dbContext.Add(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool Update(T entity)
+        {
+            try
+            {
+                db.Entry(entity).State = EntityState.Modified;
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool Delete(T entity)
+        {
+            try
+            {
+                db.Entry(entity).State = EntityState.Deleted;
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                T entity = GetEntity(id);
+                db.Entry(entity).State = EntityState.Deleted;
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+    }
+}
