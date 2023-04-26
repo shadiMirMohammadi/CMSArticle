59f547fdb9777a9f683a198cf5597384f6094e81
shadiMirMohammadi <shadi.Mir.Mohammadi.80@gmail.com>
2023-04-26 18:51:02 +0430

    feat(core) : Create the parent class and interface to the CMSArticle.ServiceLayer project
    Add two CMSArticle.RepositoryLayer and CMSArticle.ModelsLayer projects through reference to the CMSArticle.ServiceLayer project
    Create public interface IEntityService and put T type of BaseEntity type to access all model classes
    Create a public EntityService class and put the T type of the BaseEntity type to access all model classes
    Class and interface must be generic to access all model classes

---
+++ CMSArticle.ServiceLayer/EntityService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CMSArticle.RepositoryLayer;
+using CMSArticle.ModelsLayer;
+using CMSArticle.ModelsLayer.Context;
+
+namespace CMSArticle.ServiceLayer
+{
+    public class EntityService<T> : GenericRepository<T> where T : BaseEntity
+    {
+        public EntityService(CMSContext context) : base(context)
+        {
+        }
+    }
+}
