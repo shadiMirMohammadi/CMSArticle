251b094b4f204d9aa4ef9a6063ecccf85e59fade
shadiMirMohammadi <shadi.Mir.Mohammadi.80@gmail.com>
2023-08-21 16:41:44 +0330

    feat :  add AutoMapperConfig
    It is used to map data from one object to another in web development
     It acts as a mapping between two objects and converts one type of object to another

---
+++ CMSArticle/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using CMSArticle.ModelsLayer;
+using CMSArticle.Views.ViewModels;
+
+namespace CMSArticle.App_Start
+{
+    public class AutoMapperConfig
+    {
+        public static IMapper mapper;
+
+        public static void Configuration()
+        {
+            MapperConfiguration configuration = new MapperConfiguration(t =>
+            {
+                t.CreateMap<Category,CategoryViewModel>().IgnoreAllPropertiesWithAnInaccessibleSetter();
+                t.CreateMap<CategoryViewModel,Category>().IgnoreAllPropertiesWithAnInaccessibleSetter();
+
+                t.CreateMap<Article, ArticleViewModel>().IgnoreAllPropertiesWithAnInaccessibleSetter();
+                t.CreateMap<ArticleViewModel, Article>().IgnoreAllPropertiesWithAnInaccessibleSetter();
+
+                t.CreateMap<Comment, CommentViewModel>().IgnoreAllPropertiesWithAnInaccessibleSetter();
+                t.CreateMap<CommentViewModel, Comment>().IgnoreAllPropertiesWithAnInaccessibleSetter();
+            });
+            mapper = configuration.CreateMapper();
+        }
+    }
+}
--- CMSArticle/Global.asax.cs
+++ CMSArticle/Global.asax.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using GSD.Globalization;
+using System.Threading;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Optimization;
@@ -17,7 +19,20 @@ namespace CMSArticle
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
             BundleConfiguration.RegisterBundle(BundleTable.Bundles);
+
+
+            AutoMapperConfig.Configuration();
         }
 
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            var persianCulture = new PersianCulture();
+            persianCulture.DateTimeFormat.ShortDatePattern = "yyyy/MM/dd";
+            persianCulture.DateTimeFormat.LongDatePattern = "dddd d MMMM yyyy";
+            persianCulture.DateTimeFormat.AMDesignator = "ق.ظ";
+            persianCulture.DateTimeFormat.PMDesignator = "ب.ظ";
+            Thread.CurrentThread.CurrentCulture = persianCulture;
+            Thread.CurrentThread.CurrentUICulture = persianCulture;
+        }
     }
 }
